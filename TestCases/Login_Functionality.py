646745f77c0400f7151d6ddfb92d25b615b8e113
Amar Arora <149589194+AmarArora-2@users.noreply.github.com>
2025-01-21 14:09:49 +0530

    amar new

--- TestCases/Login_Functionality.py
+++ TestCases/Login_Functionality.py
@@ -32,20 +32,15 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-
-
-
 class TestCase:
-    @pytest.mark.parametrize("username,password,exp",[("a33131678@gmail.com","ProjectOdoo118","Inbox")])
-    def test_loging(self,username,password,exp):
-        driver=webdriver.Chrome()
-        driver.get("http://localhost:8069/web/login")
-        driver.find_element(By.NAME, "login").send_keys(username)
-        driver.find_element(By.NAME, "password").send_keys(password)
-        driver.find_element(By.XPATH, '//*[@class="btn btn-primary"]').click()
-       
-        act = driver.title
-        assert act==exp
+    driver=webdriver.Chrome()
+    driver.get("http://localhost:8069/web/login")
+    driver.find_element(By.NAME, "login").send_keys("a33131678@gmail.com")
+    driver.find_element(By.NAME, "password").send_keys("ProjectOdoo118")
+    driver.find_element(By.XPATH, '//*[@class="btn btn-primary"]').click()
+    exp="Inbox"
+    act = driver.title
+    assert act==exp
 
 
 
