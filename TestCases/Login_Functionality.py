b610c6db2444ac40bbb4fc6b70c6a5fb4fc1fa5f
Amar Arora <149589194+AmarArora-2@users.noreply.github.com>
2025-01-21 13:47:28 +0530

    Update Login_Functionality.py

--- TestCases/Login_Functionality.py
+++ TestCases/Login_Functionality.py
@@ -11,22 +11,20 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
 print(driver.title)
 
-try:
-    driver.get("http://localhost:8069/web/login")
-    time.sleep(5)
+driver.get("http://localhost:8069/web/login")
+time.sleep(5)
 
-    textbox1 = driver.find_element(By.NAME, "login")
-    textbox1.send_keys("a33131678@gmail.com")
-    time.sleep(2)
+textbox1 = driver.find_element(By.NAME, "login")
+textbox1.send_keys("a33131678@gmail.com")
+time.sleep(2)
 
-    textbox2 = driver.find_element(By.NAME, "password")
-    textbox2.send_keys("ProjectOdoo118")
-    time.sleep(2)
+textbox2 = driver.find_element(By.NAME, "password")
+textbox2.send_keys("ProjectOdoo118")
+time.sleep(2)
 
-    click = driver.find_element(By.XPATH, '//*[@class="btn btn-primary"]')
-    click.click()
-    time.sleep(2)
-
-    assert driver.title = "Inbox", "not the correct way"
+click = driver.find_element(By.XPATH, '//*[@class="btn btn-primary"]')
+click.click()
+time.sleep(2)
+assert driver.title = "Inbox"
 
 
